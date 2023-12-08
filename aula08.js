80fbb50f000c99ae9c1088a501f2f2f38c5cf2e7
Ryhan07 <sadninjaa2004@gmail.com>
2023-12-08 20:26:05 -0300

    Atualizado

---
+++ aula08.js
@@ -0,0 +1,15 @@
+// 
+
+const documentoIndentidade = {
+    nome:'Ed Carlos',
+    sobrenome: 'Confessor',
+    rg: '23.538.448-5',
+    cpf: '123.548.325-71',
+    time:'São paulo FC maior do BR',
+    brasileiro: true,
+    idade:24,
+
+}
+
+console.log(documentoIndentidade);
+
